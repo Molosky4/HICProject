a93b8b993cfec782378ca75682acb613eaa01141
Dylan Strnad <dylanstrnad@outlook.com>
2025-11-17 20:29:39 -0500

    Locations page additions
    locates are now displayed, with images associate with each location. Also, some changes to database connection

--- Proj/app.py
+++ Proj/app.py
@@ -1,15 +1,8 @@
 from flask import Flask, flash, redirect, render_template, request, jsonify, url_for, session
-import psycopg2 #install: pip install psycopg2
+from config import get_connection
 
 #DB connection from config
 app = Flask(__name__)
-
-# Connect to the database         //replace with your local db name
-conn = psycopg2.connect(database="flask_carrental_db", user="postgres",
-                        password="new_password", host="localhost", port="5432")
-                        #set to your password
-
-
 
 @app.route('/')
 def home():
@@ -17,7 +10,41 @@ def home():
 
 @app.route('/locations')
 def locations():
-    return render_template('locations.html')
+    conn = get_connection()
+    cur = conn.cursor()
+
+    # Fetch all locations
+    cur.execute("SELECT location_id, location_name, street, city, location_image FROM \"Locations\";")
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    locations = [
+        {"id": r[0], "name": r[1], "street": r[2], "city": r[3], "image":r[4]}
+        for r in rows
+    ]
+
+    return render_template('locations.html', locations=locations)
+
+# @app.route('/locations/<int:location_id>')
+# def cars_at_location(location_id):
+#     conn = get_connection()
+#     cur = conn.cursor()
+
+#     # adjust column/table names to your DB
+#     cur.execute("""
+#         SELECT car_id, car_make, car_model, car_year
+#         FROM "Cars"
+#         WHERE location_id = %s;
+#     """, (location_id,))
+
+#     cars = cur.fetchall()
+
+#     cur.close()
+#     conn.close()
+
+#     return render_template("results.html", cars=cars)
 
 if __name__ == '__main__':
     app.run()
--- Proj/config.py
+++ Proj/config.py
@@ -1,9 +1,9 @@
 import secrets
 import psycopg2
 
-
-conn = psycopg2.connect(database="HIC_db", user="postgres",
-                        password="359846Cb", host="localhost", port="5432")
+def get_connection():
+    return psycopg2.connect(database="flask_carrental_db", user="postgres",
+                            password="new_password", host="localhost", port="5432")
 # !!ADD THIS FILE TO GIT IGNORE ONCE WE START USING DATABASE!!
 
 #This is where well do database connections
