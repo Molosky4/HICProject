af293549353d8e36db414f6061463920ccc03e1d
CRB112 <connorrb1111@gmail.com>
2025-12-01 16:21:10 -0500

    Merge Conflicts

--- Proj/config.py
+++ Proj/config.py
@@ -2,13 +2,8 @@ import secrets
 import psycopg2
 
 def get_connection():
-<<<<<<< HEAD
     return psycopg2.connect(database="HIC_db2", user="postgres",
                         password="359846Cb", host="localhost")
-=======
-    return psycopg2.connect(database="test_db", user="postgres",
-                        password="new_password", host="localhost", port="5432")
->>>>>>> 2c1ac99ff6d0038deb7cf974f5bfb0bba6c12328
 # !!ADD THIS FILE TO GIT IGNORE ONCE WE START USING DATABASE!!
 
 #This is where well do database connections
