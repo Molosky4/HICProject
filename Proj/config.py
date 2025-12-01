748c773e1c0b6d64a6f127032d69c65a10d6c53a
Meheraj Khatri <mkhatri5@kent.edu>
2025-12-01 00:05:50 -0500

    Update

--- Proj/config.py
+++ Proj/config.py
@@ -2,7 +2,7 @@ import secrets
 import psycopg2
 
 def get_connection():
-    return psycopg2.connect(database="HIC_db", user="postgres",
+    return psycopg2.connect(database="carrental", user="postgres",
                         password="359846Cb", host="localhost", port="5432")
 # !!ADD THIS FILE TO GIT IGNORE ONCE WE START USING DATABASE!!
 
